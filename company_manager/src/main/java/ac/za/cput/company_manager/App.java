9dcae45f4c10415f67be21e8a93673be7f77ed1f
Riyaaz <riyaaz.rawoot@gmail.com>
2015-04-18 09:39:59 +0200

    Assignment 6

---
+++ company_manager/src/main/java/ac/za/cput/company_manager/App.java
@@ -0,0 +1,13 @@
+package ac.za.cput.company_manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ company_manager/src/main/java/ac/za/cput/company_manager/domain/Order.java
@@ -0,0 +1,10 @@
+package ac.za.cput.company_manager.domain;
+
+/**
+ * /**
+ * Author: Braedy Thebus (213039168)
+ * Email: Bthebus2@gmail.com
+ * Date Created:2015/04/17.
+ */
+public class Order {
+}
